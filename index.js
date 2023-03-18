9c95cd3d6e9998f8d66ca541a9cf05d496d7d36f
Mang <mangemmanuel07@gmail.com>
2023-03-18 13:26:11 +0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
+let num1 = 2;
+let num2 = 31;
+let num3 = 24;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 100) + 1;
+console.log(random); // output: a random integer between 1 and 100
+let num4 = 64
+let mod = num3 % 5;
+console.log(mod); // output: 4
+let max = Math.max(1, 5, 10, 20, 3);
+console.log(max); // output: 20
 
-// Write your code here
+
+
+
+
+
+
+
+
+
+
+
+
+ /*
+ const random = Math.random();
+ 
+ let a = 10;
+ let b = 20;
+ 
+ let min = Math.min(a, b);
+ console.log(`Min of ${a} and ${b} is ${min}`);
+ 
+ let max = Math.max(a, b);
+ console.log(`Max of ${a} and ${b} is ${max}`);
+
+ */
